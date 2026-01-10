bdae00df21f3d343c691c67d0cecb31cc92a03ff
Muhammad Ayan <mohd.ayanasim.786@gmail.com>
2026-01-10 13:59:03 +0200

    Add Tailwind to React-JS

---
+++ 07-Tailwind_CSS_with_ReactJS/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-screen w-full bg-sky-500 flex items-center justify-center text-3xl font-bold'>
+      <h2 className='text-white text-6xl'>Tailwind</h2>
+    </div>
+  )
+}
+
+export default App
